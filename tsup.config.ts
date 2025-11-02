cbae26d161750cb9b6eca1076acdd463f4e002a6
sakilanwar60 <sakilanwar60@gmail.com>
2025-11-02 15:18:19 +0600

    Update package.json and tsup.config.ts for module exports and TypeScript declaration generation
    - Simplified module exports in package.json for ESM and CommonJS.
    - Added sideEffects flag to package.json for better tree-shaking.
    - Enhanced tsup.config.ts to resolve TypeScript declaration files and specify output extensions for module formats.

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,11 +3,18 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
-  dts: true,
+  dts: {
+    resolve: true,
+  },
   splitting: false,
   sourcemap: true,
   clean: true,
   treeshake: true,
   outDir: "dist",
+  outExtension({ format }) {
+    return {
+      js: format === "cjs" ? ".js" : ".mjs",
+    };
+  },
 });
 
